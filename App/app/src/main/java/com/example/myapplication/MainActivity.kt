b30ad2308e68e76de9920e89c7a2ecbc00f1dbf4
barbrete <87492320+barbrete@users.noreply.github.com>
2024-12-12 10:37:44 -0300

    Create Coffe class, DAO class, and CRUD in Main
    - Creates the Coffe class to model cafe data
    - Implements DAO for managing data access (create, read, update, delete)
    - Adds CRUD functionality in MainActivity to interact with the Coffe data

---
+++ App/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,223 @@
+package com.example.myapplication
+
+import com.example.myapplication.database.DAO
+import com.example.myapplication.model.Cafe
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import android.widget.Toast
+import androidx.compose.material3.Button
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GerenciandoCafezinho(applicationContext)
+        }
+    }
+}
+}
+
+@Composable
+fun GerenciandoCafezinho(contexto : Context) {
+    var estadoTextFieldId by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldNome by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldNota by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldAroma by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldAcidez by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldAmargor by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldSabor by remember { mutableStateOf(TextFieldValue()) }
+    var estadoTextFieldPreco by remember { mutableStateOf(TextFieldValue()) }
+    val estadoListaCafezinho = remember { mutableStateOf<List<String>>(emptyList()) }
+
+    val banco: DatabaseReference = Firebase.database.reference
+    val dao = DAO(banco)
+
+    fun atualizarListaCafezinho() {
+        dao.mostrarDados { lista ->
+            estadoListaCafezinho.value = lista
+        }
+    }
+
+    atualizarListaCafezinho()
+
+    fun limparCampos() {
+        estadoTextFieldId = TextFieldValue()
+        estadoTextFieldNome = TextFieldValue()
+        estadoTextFieldNota = TextFieldValue()
+        estadoTextFieldAroma = TextFieldValue()
+        estadoTextFieldAcidez = TextFieldValue()
+        estadoTextFieldAmargor = TextFieldValue()
+        estadoTextFieldSabor = TextFieldValue()
+        estadoTextFieldPreco = TextFieldValue()
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Controle de Cafes", fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = estadoTextFieldId,
+            onValueChange = { estadoTextFieldId = it },
+            label = { Text("Digite um valor") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = estadoTextFieldNome,
+            onValueChange = { estadoTextFieldNome = it },
+            label = { Text("Nome") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = estadoTextFieldNota,
+            onValueChange = { estadoTextFieldNota = it },
+            label = { Text("Nota") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = estadoTextFieldAroma,
+            onValueChange = { estadoTextFieldAroma = it },
+            label = { Text("Aroma") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = estadoTextFieldAcidez,
+            onValueChange = { estadoTextFieldAcidez = it },
+            label = { Text("Acidez") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = estadoTextFieldAmargor,
+            onValueChange = { estadoTextFieldAmargor = it },
+            label = { Text("Amargor") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = estadoTextFieldSabor,
+            onValueChange = { estadoTextFieldSabor = it },
+            label = { Text("Sabor") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = estadoTextFieldPreco,
+            onValueChange = { estadoTextFieldPreco = it },
+            label = { Text("Preco") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    var cafe = Cafe(estadoTextFieldId.text,estadoTextFieldNome.text,estadoTextFieldNota.text,
+                        estadoTextFieldAroma.text.toInt() ,estadoTextFieldAcidez.text.toInt(),estadoTextFieldAmargor.text.toInt(),
+                        estadoTextFieldSabor.text.toInt() , estadoTextFieldPreco.text.toDouble())
+
+                    //Inserção dos dados no banco.
+                    dao.inserir_atualizar(cafe)
+
+                    limparCampos()
+
+                    atualizarListaCafezinho()
+
+                    Toast.makeText(contexto,"Inserido com sucesso",Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Adicionar")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = {
+                    var cafe = Cafe(estadoTextFieldId.text,estadoTextFieldNome.text,estadoTextFieldNota.text,
+                        estadoTextFieldAroma.text.toInt() ,estadoTextFieldAcidez.text.toInt(),estadoTextFieldAmargor.text.toInt(),
+                        estadoTextFieldSabor.text.toInt() , estadoTextFieldPreco.text.toDouble())
+
+                    //Inserção dos dados no banco.
+                    dao.inserir_atualizar(cafe)
+
+                    limparCampos()
+
+                    atualizarListaCafezinho()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Atualizar")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = {
+                    dao.excluir(estadoTextFieldId.text)
+
+                    limparCampos()
+
+                    atualizarListaCafezinho()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Excluir")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(estadoListaCafezinho.value) { item ->
+                Text(
+                    text = item,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    color = Color.Blue
+                )
+            }
+        }
+    }
+}
---
+++ App/app/src/main/java/com/example/myapplication/database/DAO.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication.database
+
+import android.util.Log
+import com.example.myapplication.model.Cafe
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.gson.Gson
+
+class DAO (banco : DatabaseReference){
+    var banco : DatabaseReference
+    init{
+        this.banco = banco
+    }
+    fun inserir_atualizar(cafe : Cafe){
+        this.banco.child(cafe.Id).setValue(cafe)
+    }
+
+    fun mostrarDados(callback: (ArrayList<String>) -> Unit) {
+        val listaCafes = ArrayList<String>()
+
+        this.banco.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val gson = Gson()
+                    Log.i("TESTE", "------")
+                    for (i in snapshot.children) {
+                        val json = gson.toJson(i.value)
+                        val cafe = gson.fromJson(json, Cafe::class.java)
+                        listaCafes.add(cafe.toString())
+                        Log.i("TESTE", "Cafe: " + cafe.toString())
+                    }
+                    Log.i("TESTE", "------")
+                    callback(listaCafes)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("Teste", "Erro: $error")
+            }
+        })
+    }
+    fun excluir(registro : String){
+        this.banco.child(registro).removeValue()
+    }
+}
